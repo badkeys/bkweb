6d6e7560f9eb52263dbf0dff838ff6b9ad6b28a9
Hanno Böck <hanno@gentoo.org>
2022-05-29 21:11:20 +0200

    add static htdocs content

---
+++ htdocs/js/key.js
@@ -0,0 +1,62 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("ok").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEA3+7n3nVVncZkM25RdNknGuAfkKMwcYaWqQGN4C01zGQlpJvDmXDd
+VM+YNi4pZwRACs21V+h6LLy7mieYCWZy9NV/AfjovxZEInrCX3nyHgBxtYaV+bta
+jFzFE4T3PJvuE5hc6Kx/KawU1NvE+kvQAirGDcVQe500rEcePgdezBh9wSA0yk/q
+Pbc8hgrR6z255RGodzLgJi6v7uaXdvQlhDuIwjITIcdakspIX09xdCI1IzLkEdHX
+dr4gutR8/D0UoR3RSfiWLwc4AcdAGvJ0X0HK3tCawPFxGoU3YkLIOIa0XYHTlIqu
+Dyq3WaxIJjM/tDRYlBbYl1UF+qHcy4BDjQIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("roca").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAkTKtIR/MDftNrkgyashkwifks5Swm9HEkw4ZcCijyQdpTStInk9p
+dUrq4zjG6tk5xX9AaYXLu47fmiBVzClGvAXTZbpn6JbG1IERUs+i0pBRT5CzUu7v
+J/63L72NzVvJFaT0SUE5zByhCjJE+pflMnX1e1UptOdnN5AHeY4RKkugVse3XO2x
+Ms3yH3vTyZdtnaOIFRU0d6E7PrsgRtEAqFHcAGmPUw0ZOW8A++8sS/FlnrxMf9lV
+a1/8k/A8otPfrNIipfAoq0MKJ+oxE2wML1DDxIGTGmNaN3gPANJyeuca7ptA+NGq
+5oU1x3pWMXgSvrVSXUooNk8xIkrKxSz3LQIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("smallfactors").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAp8qz/SX4rPy4d48/J9ejq7KBHokrV73jHpgkh5vGHUx2TPDOF056
+praiZ+lG2MXikOr5jfzoqyphzIj///8GVZs1h9mdp2AQplXUg+Pb39pZ/TZ1EryA
+kqvNb8V71RrPtclZRkKJHm1ob2P/A/Bf0pSxkh6z0W6zm+iDfvqOrpvbsP8hxq02
+jeGrqPTEtzMX4sm4yMlf3xGEBF9HOaYsOE3TNlhll1qhMH+RFdXUTxnHtEHQ/iiy
+oWmZjGVK2Zp7Q8+XpwxhW+F19dR29n/4yYtFu9WNA2OhoXxA5y+FYxoJg0Jl8Kql
+/RD9026hAlXbbr4mImImsUDiIo5TGVxv3QIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("pattern").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAqAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABHMgAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+AAAAAAAAAAAAAAAAAAAAAAAAAAAAAATjsQIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("debianweak").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAwJZTDExKND/DiP+LbhTIi2F0hZZt0PdX897LLwPf3+b1GOCUj1OH
+BZvVqhJPJtOPE53W68I0NgVhaJdY6bFOA/cUUIFnN0y/ZOJOJsPNle1aXQTjxAS+
+FXu4CQ6a2pzcU+9+gGwed7XxAkIVCiTprfmRCI2vIKdb61S8kf5D3YdVRH/Tq977
+nxyYeosEGYJFBOIT+N0mqca37S8hA9hCJyD3p0AM40dD5M5ARAxpAT7+oqOXkPzf
+zLtCTaHYJK3+WAce121Br4NuQJPqYPVxniUPohT4YxFTqB7vwX2C4/gZ2ldpHtlg
+JVAHT96nOsnlz+EPa5GtwxtALD43CwOlWQIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("fermat").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEAkGbTtoSrOJmrwu9FV3A3xDfxNn3EnSO94M9+QIL3qJjjIMh30xrA
+dra5ZsXjhJrRApI0X+qArvTQVv6/n9YqEoAPmF7n46yJ1TtYETFzGCjqk1sDZ8rV
+Enq4bxPHk0EQcFiQIn6iwTPOjeidD2WKUNIVeJWI/2X8Ms3ITdt+P4jV0DVeA6rd
+R31ClG+HGos1OdTA+8nMwDId0jA/Kv8MOPFBLf24p9OB8fzXP2Uno0CSSrZLktJC
+ouOcPzGgBB5sZZFHxpsrf76uJNULBrjC9Pbxx3ogpGVy1L5CC9v8qk3L44qQpi4g
+/PC9wWNsK+NPr/tiq4+S3uB369Ye1n+0ywIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("2049").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCgKCAQEBWTyUOuiyrAnulzRBN8x3FlzdzaRGaa1DbNUpCaFNUlZBzvJUomP3
+wfSK44CoC84nTM/fK0Q5nKEwls0nycRciAnNlawuBkEt6o3Tmw0aWcGxbQoJ9lVK
+oXrG1yUm4nvypt67APitktGqh4ew3vsa94PtZOpW3X4QYFcysPeQTyTwZRunTIcx
+DZCuUYi3z7tMdUlLPw5iS3sLxinJ6IK+KV1f+5nPxueA4u3Li/xbc4zOimjcfamP
+exIOpnGpbcrigmhTzVNQRZKy43WmKdCNHNkVorzJTn34jQxZ20ImgkpH8oWBCc8f
+SNGBR2tvSyYvV76BopMcj0t3+VpME5cUlwIDAQAB
+-----END RSA PUBLIC KEY-----`});document.getElementById("e3").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MIIBCAKCAQEAyfPe/hnGvfpGZKScWzwJHn7E8sqzc7Ou5bf+DLLKmHF3TD5w4rHc
+8IYS/eH+2pNzyot8cxQzp3iVXKMc8vPWpNYI30ZvEN4+5zOzXR+k3DErL0AIVHT5
+KEIwhuIW9oEtq7mQIeaevZeYc7iu1gNAPelzWKE7DYsjAif+92qyQqB+SuYxOPqg
+vrqB+T6sWBo6difvlJTq1ai/1UTAFVlldBErDObcZBslfUZuCEEv7ZKH3AOT++vB
++8ID7U8j6L3/Agc4hg2waauUl9gsuG8qTKZnYYJU09KkqXLbrM3NnyvRQQUwRbCc
+gwiC17UdQU0eEfEt4BV0E03i57PtHTj8jwIBAw==
+-----END RSA PUBLIC KEY-----`});document.getElementById("512").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN RSA PUBLIC KEY-----
+MEgCQQDf3ZUn4aBDoJdJ6dJM1X0pZlz8LWf9QD0wYsB9rAahvoSNN3JVi+xRBiI/
+7UDBtWAqeun5h44V43Rx9QJVlPYdAgMBAAE=
+-----END RSA PUBLIC KEY-----`});document.getElementById("ed25519").addEventListener("click",function(){document.getElementById("keyfield").value=`-----BEGIN PUBLIC KEY-----
+MCowBQYDK2VwAyEAGb9ECWmEzf6FQbrBZ9w7lshQhqowtrbLDFw4rXAxZuE=
+-----END PUBLIC KEY-----`});document.getElementById("clear").addEventListener("click",function(){document.getElementById("keyfield").value=""})});function fillWith(k){document.getElementById("keyfield").value=p[k]}function clearForm(){document.getElementById("keyfield").value=""}
